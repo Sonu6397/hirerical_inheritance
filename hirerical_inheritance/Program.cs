ac5431491e55603929bb4ff369f002b597ec0f61
admin <admin@CHETUIWK1860>
2022-07-27 12:50:53 +0530

    Add project files.

---
+++ hirerical_inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace hirerical_inheritance
+{
+    class Employee
+    {
+        public int EmpNo;
+
+        public void Show()
+        {
+            Console.WriteLine("EmpNo="+EmpNo);
+        }
+    }
+
+    class Student : Employee
+    {
+        public int Fees;
+
+        public void MyMethod()
+        {
+            Console.WriteLine("Fees="+Fees);
+        }
+    }
+
+    class Demo : Employee
+    {
+        public string EmpName;
+
+        public void Add()
+        {
+            Console.WriteLine("EmpName="+EmpName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d = new Demo();
+            d.EmpNo = 12;
+            d.EmpName = "sonu kuamr";
+            Student s = new Student();
+            s.Fees = 1000;
+            d.Show();
+            s.MyMethod();
+            d.Add();
+            Console.ReadLine();
+
+            
+        }
+    }
+}
